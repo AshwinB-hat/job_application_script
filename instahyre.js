5d1e6a9cddcc1f724eef20a31c4225824b863b27
Ashwin Bhat <ashwinbhat2906@gmail.com>
2022-07-01 14:21:16 +0300

    Update to the Script

--- instahyre.js
+++ instahyre.js
@@ -1,5 +1,3 @@
-```
-
 function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
    }
@@ -8,13 +6,16 @@ function sleep(ms) {
 async function jobApply() {
 for (let i = 0; i < 30; i++) {
   a = document.getElementsByClassName("application-modal-wrap")[0].children[2].children[1].children[0]
-  angular.element(a).triggerHandler("click")
+  angular.element(a).triggerHandler("click”) 
+  try {
   await sleep(1000)
   b = document.getElementsByClassName("application-modal-wrap")[4].children[2].children[0].children[0].children[0]
   angular.element(b).triggerHandler("click")
+  } catch(err){
+	  console.log("No Secondary Pop up, Wait 2 Seconds")
+  }
   await sleep(2000)
 }
 } 
 
 jobApply()
-```
