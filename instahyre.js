98e7d45ef93d7a0def2d157429638cc825db83ac
Ashwin Bhat <ashwinbhat2906@gmail.com>
2022-07-01 14:23:35 +0300

    updated closing quotes

--- instahyre.js
+++ instahyre.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 async function jobApply() {
 for (let i = 0; i < 30; i++) {
   a = document.getElementsByClassName("application-modal-wrap")[0].children[2].children[1].children[0]
-  angular.element(a).triggerHandler("click”) 
+  angular.element(a).triggerHandler("click") 
   try {
   await sleep(1000)
   b = document.getElementsByClassName("application-modal-wrap")[4].children[2].children[0].children[0].children[0]
